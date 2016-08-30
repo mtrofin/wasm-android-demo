41bd4365227d003b99a0b9d830d46822a862a7b1
clarkchenwang <clarkchenwang@google.com>
2016-08-30 12:14:13 -0700

    Restructure

--- android/app/src/main/java/moreteapots/MoreTeapotsApplication.java
+++ android/app/src/main/java/moreteapots/MoreTeapotsApplication.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.sample.moreteapots;
+package moreteapots;
 
 import javax.microedition.khronos.opengles.GL10;
 
--- android/app/src/main/java/moreteapots/MoreTeapotsNativeActivity.java
+++ android/app/src/main/java/moreteapots/MoreTeapotsNativeActivity.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.sample.moreteapots;
+package moreteapots;
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
